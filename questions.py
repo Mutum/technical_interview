134c24f05d51859a773261af0265dfb35e7ecd36
Jason Osajima <josajima@gmail.com>
2016-08-16 17:15:06 -0700

    feat: added solution to question 3

--- questions.py
+++ questions.py
@@ -67,3 +67,81 @@ print question2(b)
 print question2(c)
 #Should return 'aba'
 print question2(d)
+
+
+#Code solution for question 2
+#I had a lot of trouble with this one. Any feedback would be greatly appreciated!
+
+
+def sortbyweight(graph):
+    #Make a list of edges based on weights in ascending order
+    edge_list = []
+    for node1 in graph:
+        for i in range(len(graph[node1])):
+            edge = sorted([node1, graph[node1][i][0], graph[node1][i][1]])
+            if edge not in edge_list:
+                edge_list.append(edge)
+    edge_list = sorted(edge_list)
+
+    return edge_list
+
+def cyclic(graph):
+    #check to see if the graph with the added edge forms a cycle.
+    #Make a dict with keys as nodes and values as sets of the nodes it's connected to.
+    nodes = []
+    g = {}
+    for node in graph:
+        nodes.append(node)
+        for i in range(len(graph[node])): 
+            g[node] = set(graph[node][i][0]) 
+    
+    def dfs(graph, start):
+        visited, stack = set(), [start]
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                stack.extend(graph[vertex] - visited)
+        return(len(visited) == len(nodes))
+    
+    
+    dfs(g, nodes[0])
+    
+def question3(graph):
+    #Sort edges by weight
+    edge_list = sortbyweight(graph)
+    N = len(edge_list)
+    edge_count = 0
+    #Initialize an empty minimum spanning tree graph
+    mst = {}
+    for node in graph:
+        mst[node] = []
+    
+    
+    #If # of edges in mst == N-1, stop loop        
+    while edge_count < N-1:
+        #Add the lowest weighted edge and check to see if the graph is cyclical. If so, remove edge.
+        for edge in edge_list:
+            #Add the edge to the mst
+            for node in mst:
+                if node == edge[1]:
+                    mst[node].append((edge[2], edge[0]))
+                if node == edge[2]:
+                    mst[node].append((edge[1], edge[0]))
+                #Check to see if the graph is cyclical
+                if cyclic(mst):
+                    #If it is, remove the edge
+                    for node in mst:
+                        if node == edge[1]:
+                            mst[node].remove((edge[2], edge[0]))
+                        if node == edge[2]:
+                            mst[node].remove((edge[1], edge[0]))
+                else:
+                    edge_count += 1
+        return mst
+
+a = {'A': [('B', 2), ('C', 6), ('B', 100)],
+ 'B': [('A', 2), ('C', 5)],
+ 'C': [('B', 5), ('A', 6)]}
+
+question3(a)
